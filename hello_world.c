981547dcd1a7b0e1685af1484c95a21c6116d2c9
automaticaddison <38732101+automaticaddison@users.noreply.github.com>
2018-07-07 12:38:18 -0700

    Initial commit

---
+++ hello_world.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *  @file           hello_world.c
+ *  @author         Addison Sears-Collins
+ *  @date           7/7/2018  
+ *  @version        1.0 
+ *  
+ *  @brief	    Prints "hello, world" to the console
+ *
+ *  Purpose: This program is my first C program. It prints "hello, world" 
+ *           to the console.
+ *****************************************************************************/
+ // Include information about the standard library
+#include <stdio.h> 
+
+// The main method that drives the program
+int main() {  
+
+  // Call library function printf. \n represents the new line character
+  printf("hello, world\n");
+
+  // Exit the program
+  return 0;
+
+}
